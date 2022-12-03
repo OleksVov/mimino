31c2d6ac1c196ee511ae54254b7cfe20b9f079b3
OleksVov <alex.vovchok@gmail.com>
2022-12-03 12:10:33 +0200

    added modal for book room

--- js/modal-table.js
+++ js/modal-table.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[data-modal-table-open]"),
+    closeModalBtn: document.querySelector("[data-modal-table-close]"),
+    modal: document.querySelector("[data-modal-table]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
